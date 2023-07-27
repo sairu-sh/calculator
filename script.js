7ef52f8151d29bfd104902543ed08322c9d64438
sairu-sh <tmg.sairush@gmail.com>
2023-07-28 02:14:19 +0545

    did some DOM manipulation with JS did event delegation for the buttons that customize the calculator. now the active button gets enlarged and others get the normal size. selecting the rainbow button will make all the buttons the normal size. selecting the button when the rainbow button is active will select the default setting of the calculator. also did the same for the mode of calculator.

--- script.js
+++ script.js
@@ -1,10 +1,80 @@
 "use strict";
 
-var screenWidth = window.innerWidth;
-var screenHeight = window.innerHeight;
+const btns = document.querySelectorAll(".basic-button");
+const btnContainer = document.querySelectorAll(".button-container");
+const btnAll = document.querySelector(".customize");
+const frame = document.querySelector(".frame");
+const screen = document.querySelector(".screen");
+const gridItem = document.querySelector(".grid-item");
 
-console.log("Screen width: " + screenWidth);
-console.log("Screen height: " + screenHeight);
+//functions
 
-const calculator = document.querySelector(".frame");
-console.log(calculator.getBoundingClientRect());
+//setting the default calculator setting after exiting the rainbow setting
+const defaultSetting = function () {
+  btnContainer.forEach((container) =>
+    container.querySelector(".basic-button").classList.add("active")
+  );
+};
+
+//to set the active class to the elements
+const container = function (targetE, className) {
+  const clickedContainer = targetE.closest(className);
+
+  [...clickedContainer.children].forEach((child) =>
+    child.classList.remove("active")
+  );
+
+  const rainbow = targetE
+    .closest(".customize")
+    .querySelector(".rainbow-button");
+
+  if (rainbow.classList.contains("active")) {
+    rainbow.classList.remove("active");
+    defaultSetting();
+  }
+
+  targetE.classList.add("active");
+};
+
+/*
+//changing the colors in the calculator
+const borderChanger = function (element, color) {
+  window.getComputedStyle(element).border = `5px solid #${color}`;
+  console.log(element, color);
+};
+
+borderChanger("frame", "0f52ba");
+
+const updateCalc = function () {
+  window.getComputedStyle(frame).border = `hi`;
+};
+*/
+//choosing the element that was clicked
+
+//event handlers
+btnAll.addEventListener("click", function (e) {
+  if (e.target.classList.contains("basic-button")) {
+    container(e.target, ".button-container");
+    colors(e.target);
+  }
+  if (e.target.classList.contains("mode-button")) container(e.target, ".mode");
+  if (e.target.classList.contains("rainbow-button")) {
+    btns.forEach((btn) => btn.classList.remove("active"));
+    e.target.classList.add("active");
+  }
+});
+
+/*const element = function (targetE) {
+  if (targetE.closest(".border")) return "border";
+};
+
+const changeBorder = function (color) {
+  //getting the window.getComputedStyle
+  const computedStyle = window.getComputedStyle(element).elemen;
+};
+
+//changing the actual color
+const colors = function (targetE) {
+  const elementClicked = element(targetE);
+  changeBorder();
+};*/
